e0876de7c31cd154574a0542ab459a89c3fe3950
arturmihk <115464760+arturmihk@users.noreply.github.com>
2025-06-06 16:20:08 +0300

    Create kontakt.php

---
+++ Skriptid/kontakt.php
@@ -0,0 +1,30 @@
+<?php include 'config.php'; ?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="UTF-8">
+  <title>Kontakt</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <header class="bg-primary text-white p-3 text-center">
+    <h1>Kontakt</h1>
+  </header>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <div>
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="index.php">Avaleht</a></li>
+          <li class="nav-item"><a class="nav-link" href="uudised.php">Uudised</a></li>
+          <li class="nav-item"><a class="nav-link" href="tugileht.php">Tugileht</a></li>
+          <li class="nav-item"><a class="nav-link" href="kontakt.php">Kontakt</a></li>
+          <li class="nav-item"><a class="nav-link" href="admin.php">Admin</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<p>IT Tugi: it@oige.local, Tel:555 5555</p>
+</body>
+</html>
