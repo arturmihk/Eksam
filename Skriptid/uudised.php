2739b0c47fb66400f40309163584467040c90325
arturmihk <115464760+arturmihk@users.noreply.github.com>
2025-06-06 16:20:56 +0300

    Create uudised.php

---
+++ Skriptid/uudised.php
@@ -0,0 +1,30 @@
+  GNU nano 5.6.1                                                  uudised.php                                                             <?php include 'config.php'; ?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="UTF-8">
+  <title>Uudised</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <header class="bg-primary text-white p-3 text-center">
+    <h1>Uudised</h1>
+  </header>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <div>
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="index.php">Avaleht</a></li>
+          <li class="nav-item"><a class="nav-link" href="uudised.php">Uudised</a></li>
+          <li class="nav-item"><a class="nav-link" href="tugileht.php">Tugileht</a></li>
+          <li class="nav-item"><a class="nav-link" href="kontakt.php">Kontakt</a></li>
+          <li class="nav-item"><a class="nav-link" href="admin.php">Admin</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<p>* Mario tantsuklubi avaneb 10 juuli</p>
+</body>
+</html>
